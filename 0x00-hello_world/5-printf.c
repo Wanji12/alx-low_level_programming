40dddad836025c4e46b96aaac6f604dd06ab23d1
Hope Wanjiru <wanjiruhope899@gmail.com>
2023-06-18 15:23:13 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n  uuuuu);
 
-return (0);
+/**
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success.
+ */
+
+int main(void)
+
+{
+
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
+
 }
